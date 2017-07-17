a63ea5ba97a2c6a65cbb38c6e6a31b300ed161bf
brine <spreetarded@gmail.com>
2017-07-17 15:23:58 -0400

    more updates

--- thronesextractor/ThronesExtractor/MainWindow.xaml.cs
+++ thronesextractor/ThronesExtractor/MainWindow.xaml.cs
@@ -10,6 +10,8 @@ using System.Xml.Linq;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.Reflection;
+using System.Windows.Controls;
+using System.Windows.Data;
 
 namespace ThronesExtractor
 {
@@ -18,11 +20,13 @@ namespace ThronesExtractor
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<Card> cardList = new List<Card>();
+        private List<Set> setList = new List<Set>();
+
         public MainWindow()
         {
             InitializeComponent();
-            cardList = new List<Card>();
-            setList = new List<Set>();
+            LoadPropertyTable();
 
             string cardsurl = new WebClient().DownloadString("http://www.thronesdb.com/api/public/cards/");
             JArray cardsjson = (JArray)JsonConvert.DeserializeObject(cardsurl);
@@ -32,8 +36,19 @@ namespace ThronesExtractor
                 card.Name = jcard.Value<string>("name");
                 card.Pack = jcard.Value<string>("pack_code");
                 card.Id = jcard.Value<string>("octgn_id") == null ? Guid.NewGuid() : Guid.Parse(jcard.Value<string>("octgn_id"));
-                card.Properties = GetCardProperties(jcard);
-                card.Size = card.Properties["Type"] == "Plot" ? "HorizontalCards" : null;
+                foreach (var prop in Properties)
+                {
+                    var value = jcard.Value<string>(prop.DbName);
+                    if (value != null)
+                    {
+                        if (prop.Type == PropertyTypes.Bool)
+                            value = (value == "True") ? "Yes" : "No";
+                        else
+                            value = MakeXMLSafe(value);
+                        card.Properties.Add(prop, value);
+                    }
+                }
+                card.Size = card.getProperty("Type") == "Plot" ? "HorizontalCards" : null;
                 cardList.Add(card);
             }
 
@@ -47,143 +62,63 @@ namespace ThronesExtractor
                 set.Name = jset.Value<string>("name");
                 set.Code = jset.Value<string>("code");
                 set.Cards = new List<Card>(cardList.Where(x => x.Pack == set.Code));
-                set.Xml = GenerateXml(set);
                 setList.Add(set);
             }
-
+            foreach (var prop in Properties)
+            {
+                OutputGrid.Columns.Add(new DataGridTextColumn
+                {
+                    Binding = new Binding
+                    {
+                        Path = new PropertyPath("GetProperty", prop.OctgnName.ToArray()),
+                        Mode = BindingMode.OneTime
+                    },
+                    Header = prop.OctgnName
+                });
+            }
             SetsPanel.ItemsSource = setList;
         }
+                
 
-        private List<Card> cardList;
-        private List<Set> setList;
-
-        private Dictionary<string, string> GetCardProperties(JToken props)
+        private void UpdateDataGrid(object sender, RoutedPropertyChangedEventArgs<object> e)
         {
-            var ret = new Dictionary<string, string>();
-
-            ret["Type"] = props.Value<string>("type_name");
-            ret["Faction"] = props.Value<string>("faction_name");
-            ret["Text"] = MakeXMLSafe(props.Value<string>("text") ?? ""); //todo - sterilize text box
-            ret["Traits"] = props.Value<string>("traits") ?? "";
-            ret["CardNumber"] = props.Value<string>("position");
-            ret["Illustrator"] = props.Value<string>("illustrator");
-            switch (ret["Type"])
-            {
-                case "Character":
-                    ret["Unique"] = (props.Value<string>("is_unique") == "True") ? "Yes" : "No";
-                    ret["Loyal"] = (props.Value<string>("is_loyal") == "True") ? "Yes" : "No";
-                    ret["Military"] = (props.Value<string>("is_military") == "True") ? "Yes" : "No";
-                    ret["Intrigue"] = (props.Value<string>("is_intrigue") == "True") ? "Yes" : "No";
-                    ret["Power"] = (props.Value<string>("is_power") == "True") ? "Yes" : "No";
-                    ret["Cost"] = props.Value<string>("cost");
-                    ret["Strength"] = props.Value<string>("strength");
-                    break;
-                case "Location":
-                    ret["Unique"] = (props.Value<string>("is_unique") == "True") ? "Yes" : "No";
-                    ret["Loyal"] = (props.Value<string>("is_loyal") == "True") ? "Yes" : "No";
-                    ret["Cost"] = props.Value<string>("cost");
-                    break;
-                case "Event":
-                    ret["Loyal"] = (props.Value<string>("is_loyal") == "True") ? "Yes" : "No";
-                    ret["Cost"] = props.Value<string>("cost");
-                    break;
-                case "Attachment":
-                    ret["Loyal"] = (props.Value<string>("is_loyal") == "True") ? "Yes" : "No";
-                    ret["Unique"] = (props.Value<string>("is_unique") == "True") ? "Yes" : "No";
-                    ret["Cost"] = props.Value<string>("cost");
-                    break;
-                case "Plot":
-                    ret["Loyal"] = (props.Value<string>("is_loyal") == "True") ? "Yes" : "No";
-                    ret["DeckLimit"] = props.Value<string>("deck_limit");
-                    ret["PlotGoldIncome"] = props.Value<string>("income");
-                    ret["PlotInitiative"] = props.Value<string>("initiative");
-                    ret["PlotClaim"] = props.Value<string>("claim");
-                    ret["PlotReserve"] = props.Value<string>("reserve");
-                    break;
-                case "Agenda":
-                    break;
-                case "Title":
-                    break;
-            }
-
-            return ret;
+            OutputGrid.ItemsSource = ((Set)e.NewValue).Cards;
         }
 
-        private XmlDocument GenerateXml(Set set)
-        {
-            var ret = new XmlDocument();
-            ret.AppendChild(ret.CreateXmlDeclaration("1.0", "utf-8", "yes"));
-            XmlNode root = ret.CreateElement("set");
-
-            root.Attributes.Append(CreateAttribute(ret, "name", set.Name));
-            root.Attributes.Append(CreateAttribute(ret, "id", set.Id));
-            root.Attributes.Append(CreateAttribute(ret, "gameId", "30c200c9-6c98-49a4-a293-106c06295c05"));
-            root.Attributes.Append(CreateAttribute(ret, "version", "1.0.0.0"));
-            root.Attributes.Append(CreateAttribute(ret, "gameVersion", "1.0.0.0"));
-
-            ret.AppendChild(root);
-
-            XmlNode cardsNode = ret.CreateElement("cards");
-            root.AppendChild(cardsNode);
-
-            foreach (var c in set.Cards)
-            {
-                XmlNode cardNode = ret.CreateElement("card");
-                cardNode.Attributes.Append(CreateAttribute(ret, "name", c.Name));
-                cardNode.Attributes.Append(CreateAttribute(ret, "id", c.Id.ToString()));
-                if (c.Size != null)
-                {
-                    cardNode.Attributes.Append(CreateAttribute(ret, "size", c.Size));
-                }
-
-                foreach (KeyValuePair<string, string> kvi in c.Properties)
-                {
-                    XmlNode prop = ret.CreateElement("property");
-                    prop.Attributes.Append(CreateAttribute(ret, "name", kvi.Key));
-                    prop.Attributes.Append(CreateAttribute(ret, "value", kvi.Value));
-                    cardNode.AppendChild(prop);
-                }
-                cardsNode.AppendChild(cardNode);
-            }
-
-            StringWriter sw = new StringWriter();
-            XmlTextWriter tx = new XmlTextWriter(sw);
-            ret.WriteTo(tx);
-
-            string str = sw.ToString();// 
-
-            Clipboard.SetText(str);
-            return ret;
-        }
-
-        private XmlAttribute CreateAttribute(XmlDocument doc, string name, string value)
-        {
-            XmlAttribute ret = doc.CreateAttribute(name);
-            ret.Value = value;
-            return (ret);
-        }
 
         public static string MakeXMLSafe(string makeSafe)
         {
-            makeSafe = makeSafe.Replace("<b>", "<");
-            makeSafe = makeSafe.Replace("</b>", ">");
-            makeSafe = makeSafe.Replace("<i>", "<");
-            makeSafe = makeSafe.Replace("</i>", ">");
-            makeSafe = makeSafe.Replace("\n", "\r\n");
-            makeSafe = makeSafe.Replace("[thenightswatch]", "[The Night's Watch]");
-            makeSafe = makeSafe.Replace("[baratheon]", "[Baratheon]");
-            makeSafe = makeSafe.Replace("[targaryen]", "[Targaryen]");
-            makeSafe = makeSafe.Replace("[stark]", "[Stark]");
-            makeSafe = makeSafe.Replace("[martell]", "[Martell]");
-            makeSafe = makeSafe.Replace("[tyrell]", "[Tyrell]");
-            makeSafe = makeSafe.Replace("[lannister]", "[Lannister]");
-            makeSafe = makeSafe.Replace("[greyjoy]", "[Greyjoy]");
-            makeSafe = makeSafe.Replace("[intrigue]", "[Intrigue]");
-            makeSafe = makeSafe.Replace("[military]", "[Military]");
-            makeSafe = makeSafe.Replace("[power]", "[Power]");
+            makeSafe = makeSafe.Replace("&", "&amp;");
+            makeSafe = makeSafe.Replace("\n", "&#xd;&#xa;");
+            makeSafe = makeSafe.Replace("[thenightswatch]", "<s value=\"nightswatch\">[The Night's Watch]</s>");
+            makeSafe = makeSafe.Replace("[baratheon]", "<s value=\"baratheon\">[House Baratheon]</s>");
+            makeSafe = makeSafe.Replace("[targaryen]", "<s value=\"targaryen\">[House Targaryen]</s>");
+            makeSafe = makeSafe.Replace("[stark]", "<s value=\"stark\">[House Stark]</s>");
+            makeSafe = makeSafe.Replace("[martell]", "<s value=\"martell\">[House Martell]</s>");
+            makeSafe = makeSafe.Replace("[tyrell]", "<s value=\"tyrell\">[House Tyrell]</s>");
+            makeSafe = makeSafe.Replace("[lannister]", "<s value=\"lannister\">[House Lannister]</s>");
+            makeSafe = makeSafe.Replace("[greyjoy]", "<s value=\"greyjoy\">[House Greyjoy]</s>");
+            makeSafe = makeSafe.Replace("[intrigue]", "<s value=\"intrigue\">[Intrigue]</s>");
+            makeSafe = makeSafe.Replace("[military]", "<s value=\"military\">[Military]</s>");
+            makeSafe = makeSafe.Replace("[power]", "<s value=\"power\">[Power]</s>");
             return (makeSafe);
         }
 
+        private static List<Property> Properties = new List<Property>();
+
+        private static void LoadPropertyTable()
+        {
+            foreach (var propdef in XDocument.Load("propertytable.xml").Document.Descendants("property"))
+            {
+                var prop = new Property()
+                {
+                    DbName = propdef.Attribute("db_name").Value,
+                    OctgnName = propdef.Attribute("octgn_name").Value,
+                    Type = (propdef.Attribute("type") == null) ? PropertyTypes.String : PropertyTypes.Bool
+                };
+                Properties.Add(prop);
+            }
+        }
 
         private static XDocument setGuidTable = null;
 
@@ -231,8 +166,80 @@ namespace ThronesExtractor
             {
                 File.Delete(savePath);
             }
-            set.Xml.Save(savePath);
+
+            var xml = new XmlDocument();
+            xml.AppendChild(xml.CreateXmlDeclaration("1.0", "utf-8", "yes"));
+
+            XmlNode root = xml.CreateElement("set");
+            root.Attributes.Append(CreateAttribute(xml, "name", set.Name));
+            root.Attributes.Append(CreateAttribute(xml, "id", set.Id));
+            root.Attributes.Append(CreateAttribute(xml, "gameId", "30c200c9-6c98-49a4-a293-106c06295c05"));
+            root.Attributes.Append(CreateAttribute(xml, "version", "1.0.0.0"));
+            root.Attributes.Append(CreateAttribute(xml, "gameVersion", "1.0.0.0"));
+            xml.AppendChild(root);
+
+            XmlNode cardsNode = xml.CreateElement("cards");
+            root.AppendChild(cardsNode);
+
+
+            foreach (var c in set.Cards)
+            {
+                XmlNode cardNode = xml.CreateElement("card");
+                cardNode.Attributes.Append(CreateAttribute(xml, "name", c.Name));
+                cardNode.Attributes.Append(CreateAttribute(xml, "id", c.Id.ToString()));
+                if (c.Size != null)
+                {
+                    cardNode.Attributes.Append(CreateAttribute(xml, "size", c.Size));
+                }
+
+                foreach (KeyValuePair<Property, string> kvi in c.Properties)
+                {
+                    XmlNode prop = xml.CreateElement("property");
+                    prop.Attributes.Append(CreateAttribute(xml, "name", kvi.Key.OctgnName));
+                    if (kvi.Key.OctgnName == "Text")
+                    {
+                        var propdoc = new XmlDocument();
+                        propdoc.LoadXml("<root>" + kvi.Value + "</root>");
+                        foreach (XmlNode node in propdoc.FirstChild.ChildNodes)
+                        {
+                            prop.AppendChild(xml.ImportNode(node, true));
+                        }
+                    }
+                    else
+                    {
+                        prop.Attributes.Append(CreateAttribute(xml, "value", kvi.Value));
+                    }
+                    cardNode.AppendChild(prop);
+                }
+                cardsNode.AppendChild(cardNode);
+            }
+                                                
+            xml.Save(savePath);
         }
+
+
+        private XmlAttribute CreateAttribute(XmlDocument doc, string name, string value)
+        {
+            XmlAttribute ret = doc.CreateAttribute(name);
+            ret.Value = value;
+            return (ret);
+        }
+    }
+
+    public enum PropertyTypes
+    {
+        Bool,
+        String
+    }
+
+    public class Property
+    {
+        public string OctgnName { get; set; }
+        public string DbName { get; set; }
+        public PropertyTypes Type { get; set; }
+
+        public Property()
+        { }
     }
 
     public class Card
@@ -241,19 +248,24 @@ namespace ThronesExtractor
         public Guid Id { get; set; }
         public string Size { get; set; }
         public string Pack { get; set; }
-        public Dictionary<string, string> Properties { get; set; }
+        public Dictionary<Property, string> Properties { get; set; }
 
         public Card()
         {
+            Properties = new Dictionary<Property, string>();
         }
 
+        public string getProperty(string propName)
+        {
+            return Properties.First(x => x.Key.OctgnName == propName).Value;
+        }
     }
+
     public class Set
     {
         public string Name { get; set; }
         public string Id { get; set; }
         public string Code { get; set; }
-        public XmlDocument Xml { get; set; }
 
         public List<Card> Cards { get; set; }
 
